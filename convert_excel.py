5ffd6b72436f89d13b65128422414468381b8b6d
Niall O'Callaghan <nocalla@users.noreply.github.com>
2024-05-25 20:12:01 +0100

    Add function docstrings

--- convert_excel.py
+++ convert_excel.py
@@ -5,6 +5,16 @@ from fpdf import FPDF
 
 
 def read_dataframe(file_path: str, sheet_name: str) -> pd.DataFrame:
+    """
+    Read a named worksheet in an Excel file into a Pandas dataframe.
+
+    :param file_path: Path to Excel file
+    :type file_path: str
+    :param sheet_name: Name of worksheet to read
+    :type sheet_name: str
+    :return: Pandas dataframe of Excel data
+    :rtype: pd.DataFrame
+    """
     # Read the named sheet with pandas
     df = pd.read_excel(file_path, sheet_name=sheet_name)
     df.fillna(0, inplace=True)
@@ -15,9 +25,19 @@ def read_dataframe(file_path: str, sheet_name: str) -> pd.DataFrame:
     return df
 
 
-def generate_pdf(file: str, df: pd.DataFrame) -> str:
+def generate_pdf(output_filename: str, df: pd.DataFrame) -> str:
+    """
+    Create a PDF from a dataframe using the FPDF2 library.
+
+    :param output_filename: Name of the output PDF file (without extension)
+    :type output_filename: str
+    :param df: Dataframe to convert to PDF table
+    :type df: pd.DataFrame
+    :return: Path to the PDF output file
+    :rtype: str
+    """
     # Create a PDF file using fpdf2
-    pdf_file_path = f"{file}.pdf"
+    pdf_file_path = f"{output_filename}.pdf"
     df = df.map(str)
     COLUMNS = [list(df)]  # Get list of dataframe columns
     ROWS = df.values.tolist()  # Get list of dataframe rows
@@ -36,14 +56,39 @@ def generate_pdf(file: str, df: pd.DataFrame) -> str:
     return pdf_file_path
 
 
-def generate_jpg(file: str, pdf_file_path: str) -> str:
-    jpg_file_path = f"{file}.jpg"
-    command = ["pdftoppm", "-jpeg", "-singlefile", pdf_file_path, file]
+def generate_jpg(output_filename: str, pdf_file_path: str) -> str:
+    """
+    Creates a single page JPG based on the first page of an existing PDF file.
+    NB: requires the installation of poppler-utils to work
+
+    :param output_filename: Name of the output JPG file (without extension)
+    :type output_filename: str
+    :param pdf_file_path: Path to the PDF file being converted to JPG
+    :type pdf_file_path: str
+    :return: Path to the JPG output file
+    :rtype: str
+    """
+    jpg_file_path = f"{output_filename}.jpg"
+    command = [
+        "pdftoppm",
+        "-jpeg",
+        "-singlefile",
+        pdf_file_path,
+        output_filename,
+    ]
     subprocess.run(command, check=True)
     return jpg_file_path
 
 
 def main(file: str, sheet_name: str) -> None:
+    """
+    Generate a PDF and JPG version of the data in an Excel worksheet.
+
+    :param file: Name of the input Excel file (without extension)
+    :type file: str
+    :param sheet_name: Name of the Excel worksheet to read
+    :type sheet_name: str
+    """
     file_path = f"{file}.xlsx"
 
     df = read_dataframe(file_path, sheet_name)
@@ -54,7 +99,6 @@ def main(file: str, sheet_name: str) -> None:
 
 
 if __name__ == "__main__":
-    # Load the Excel file
     file = "antibiogram"
     sheet_name = "Drug Information"
     main(file, sheet_name)
